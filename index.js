6c4382e6da2975911574713308aa136078025386
Rodolfo Caldeira <me@rodolfocaldeira.com>
2015-08-10 23:45:23 +0100

    Adds the tests for the number method.

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-(function() {
+(function () {
 
 'use strict';
 
 var self = this;
-
 var readable = {};
 
 // http://stackoverflow.com/questions/9461621/how-to-format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900-in-javascrip
-readable.number = function(num, digits) {
+readable.number = function (num, digits) {
   var si = [
-    { value: 1E18, symbol: "E" },
-    { value: 1E15, symbol: "P" },
-    { value: 1E12, symbol: "T" },
-    { value: 1E9,  symbol: "G" },
-    { value: 1E6,  symbol: "M" },
-    { value: 1E3,  symbol: "k" }
+    {value: 1E12, symbol: "T"},
+    {value: 1E9, symbol: "B"},
+    {value: 1E6, symbol: "M"},
+    {value: 1E3, symbol: "k"}
   ];
   for (var i = 0; i < si.length; i++) {
     if (num >= si[i].value) {
@@ -23,36 +20,36 @@ readable.number = function(num, digits) {
     }
   }
   return num;
-}
+};
 
 // http://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
-readable.romanNumeral = function(num) {
-    if (!+num)
-        return false;
-    var digits = String(+num).split(""),
-        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-               "","I","II","III","IV","V","VI","VII","VIII","IX"],
-        roman = "",
-        i = 3;
-    while (i--)
-        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-    return Array(+digits.join("") + 1).join("M") + roman;
-}
+readable.romanNumeral = function (num) {
+  if (!+num)
+    return false;
+  var digits = String(+num).split(""),
+      key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+        "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+        "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+      roman = "",
+      i = 3;
+  while (i--)
+    roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+  return Array(+digits.join("") + 1).join("M") + roman;
+};
 
 // https://github.com/taijinlee/humanize
 // https://en.wikipedia.org/wiki/Ordinal_number_(linguistics)
-readable.ordinal = function(input) {
-  var suffix = ['th', 'st', 'nd', 'rd']; 
-  var value = input % 10; 
+readable.ordinal = function (input) {
+  var suffix = ['th', 'st', 'nd', 'rd'];
+  var value = input % 10;
   return (suffix[(value - 20) % 10] || suffix[value] || suffix[0]);
-}
+};
 
 self.rc = self.rc || {};
 self.rc.readable = readable;
 
 if (typeof module !== 'undefined') {
-  module.exports = self.rc.readable;
+  module.exports = self.rc;
 }
 
-})();
+}).call(this);
--- test/readabe_test.js
+++ test/readabe_test.js
@@ -1,6 +1,19 @@
 var rc = require('../');
 var test = require('tape');
 
-console.log(rc.number(10));
+test('number', function(t) {
+  t.equal(rc.readable.number(1, 2), 1, '1 isn\'t shortened');
+  t.equal(rc.readable.number(100, 2), 100, '100 isn\'t shortened');
+  t.equal(rc.readable.number(1E3, 2), '1k', '1000 is shortened to 1k');
+  t.equal(rc.readable.number(1E6, 2), '1M', '1000000 is shortened to 1M');
+  t.equal(rc.readable.number(1E9, 2), '1B', '10E9 is shortened to 1B');
+  t.equal(rc.readable.number(1E12, 2), '1T', '10E12 is shortened to 1T');
 
-console.log('Hello World 2');
+  t.equal(rc.readable.number(1155, 1), '1.2k', '1155 is shortened to 1.2k');
+  t.equal(rc.readable.number(1150, 2), '1.15k', '1150 is shortened to 1.15k');
+  t.equal(rc.readable.number(1155, 2), '1.16k', '1155 is shortened to 1.16k');
+
+  t.equal(rc.readable.number(123456789, 1), '1.16k', '123456789 is shortened to 123.5M');
+
+  t.end();
+});
